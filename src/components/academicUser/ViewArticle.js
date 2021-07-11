313d7106be3c4f2b9049225d0eaac09532d878c3
Chathura Wanniarachchi <wachathuraj@gmail.com>
2021-07-11 17:19:03 +0530

    Post viewing devide into another component & complete view post section
    Comment section is not there.

---
+++ src/components/academicUser/ViewArticle.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import AcademicUserGeneralNav from "./acaNavbar";
+import {makeStyles} from "@material-ui/core/styles";
+import Article from "./article";
+import UserInfo from "./writerInfo";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        width: "50%",
+    },
+    navBar: {
+        display: "block",
+        position: "absolute",
+    },
+    pageContent: {
+        paddingTop: 100,
+    },
+    article: {
+        width: "50%",
+    },
+    userInfo: {
+        left: 0,
+    },
+    ccImage: {
+        width: 100,
+        display: "block",
+        paddingTop: 20,
+        float: "right",
+        paddingRight: -80,
+    },
+    downloadButton: {
+        padding: 8,
+        color: "#fff",
+        borderRadius: 5,
+        display: "block",
+        marginTop: 20,
+        float: "left",
+        backgroundColor: "#935FF9",
+    }
+}));
+
+export default function ViewArticle() {
+    const classes = useStyles();
+    return (
+        <div>
+            <AcademicUserGeneralNav className={classes.navBar}/>
+            <div className={classes.pageContent}>
+                <Grid container spacing={3}>
+                    <Grid item xs={1}></Grid>
+                    <Grid item xs={6} className={classes.article}>
+                        <Article/>
+
+                        <Grid container spacing={3}>
+                            <Grid item xs={6}>
+                                <Button className={classes.downloadButton}><CloudDownloadIcon/> &nbsp; Download Content
+                                    as PDF</Button>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <img className={classes.ccImage}
+                                     src={'https://mirrors.creativecommons.org/presskit/buttons/88x31/png/by.png'}/>
+                            </Grid>
+                        </Grid>
+
+                        <div>
+                            comment section
+                        </div>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <UserInfo className={classes.userInfo}/>
+                        <br/>
+                    </Grid>
+                    <Grid item xs={1}></Grid>
+                </Grid>
+            </div>
+        </div>
+
+    )
+}
---
+++ src/components/academicUser/article.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import {Card, CardActionArea, CardActions} from "@material-ui/core";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    media: {
+        height: 250,
+    },
+    title:{
+        textAlign:"left",
+        fontWeight:"bold",
+    },
+    tags:{
+        margin: theme.spacing(1),
+        fontWeight:"bold",
+        borderRadius:'50px'
+    },
+    content:{
+        fontSize:20,
+        padding:20,
+        paddingTop:10,
+        textAlign:'justify',
+    },
+    previewer:{
+        width:"110%",
+    },
+    contentFooter:{
+        textAlign:"center",
+        display:"block",
+        padding:10,
+    }
+}));
+
+export default function Article(customWidth) {
+    const classes = useStyles();
+    return (
+        <div>
+            <div align="center" className={classes.previewer}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        <CardMedia
+                            className={classes.media}
+                            image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.incimages.com%2Fuploaded_files%2Fimage%2F1920x1080%2Fgetty_486803862_178266.jpg"
+                            title="Cover Image"
+                        />
+
+                        <CardContent>
+                            <Typography gutterBottom variant="h2" component="h2" className={classes.title}>
+                                Say hallo to Raspberry PI 🍓
+                            </Typography>
+                            <br/>
+                            <div>
+                                <Button color="primary" className={classes.tags}>#Science</Button>
+                                <Button color="secondary" className={classes.tags}>#Electronics</Button>
+                                <Button color="default" className={classes.tags}>#Raspberry</Button>
+                                <Button color="green" className={classes.tags}>#ComputerScience</Button>
+                            </div>
+                        </CardContent>
+
+                    </CardActionArea>
+                    <hr/>
+                    <CardActions>
+                        <div className={classes.content}>
+                            {/*article*/}
+                        </div>
+                    </CardActions>
+                    <hr/>
+                    <div className={classes.contentFooter}>
+                        <span>Repost abuse</span>
+                    </div>
+                </Card>
+            </div>
+        </div>
+    )
+}
