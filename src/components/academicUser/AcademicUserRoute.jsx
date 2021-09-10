c3601e34c1df6d2f08cd83435a9aeb89aefe310f
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-09-10 14:30:14 +0530

    Organizing the academic user folder

---
+++ src/components/academicUser/AcademicUserRoute.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import AcademicUserGeneralNav from "./navBars/acaNavbar";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {makeStyles} from "@material-ui/core/styles";
+import AcademicDashboard from "./AcademicDashboard/AcademicDashboard";
+import CreatePost from './CreatePost/CreatePost';
+import WriteArticle from './CreatePost/WriteArticle';
+import PreviewArticle from "./subComponents/PreviewArticle";
+import SearchResult from './Search/SearchResult';
+import ViewArticle from "./ViewArticle/ViewArticle";
+import UniversityProfile from "../moderator/UniversityProfile";
+import AcademicHome from "./AcademicHome";
+import UploadMedia from "./CreatePost/UploadMedia";
+import ArticleVersioning from "./CreatePost/ArticleVersioning";
+import {user} from "../auth/auth";
+import Navigationbar from "../navbar";
+import GenNavbar from "./navBars/genNavbar";
+import AllNotifications from "./Notifications/AllNotifications";
+import UpdateProfile from "./ProfileUpdate/UpdateProfile";
+import ProfileInfo from './ProfileInfo';
+import AuthorProfile from "./AuthorProfile";
+
+const useStyles = makeStyles((theme) => ({
+    navBar: {
+        display: "block",
+        position: "absolute",
+    },
+}));
+
+export default function AcademicUserRoute() {
+    const classes = useStyles();
+
+    let userID = ""
+    let userRole = "";
+    if (user()) {
+        userID = user()._id;
+        userRole = user().role;
+    }
+
+    return (
+        <Router>
+            {userRole === "" ? (
+                // non login user
+                <Navigationbar/>
+            ) : (
+                userRole === "academic" ? (
+                    // academic user
+                    <AcademicUserGeneralNav className={classes.navBar}/>
+                ) : (
+                    // general lodged in user
+                    <GenNavbar/>
+                )
+            )}
+            {console.log(window.location.href)}
+            <Switch>
+                <Route path="/components/academicUser/" exact component={AcademicHome}/>
+                <Route path="/components/academicUser/tagLookup" component={AcademicHome}/>
+                <Route path="/components/academicUser/academicDashboard" component={AcademicDashboard}/>
+                <Route path="/components/academicUser/createPost" component={CreatePost}/>
+                <Route path="/components/academicUser/writeArticle" component={WriteArticle}/>
+                <Route path="/components/academicUser/articleVersioning" component={ArticleVersioning}/>
+                <Route path="/components/academicUser/uploadMedia" component={UploadMedia}/>
+                <Route path="/components/academicUser/previewPost" component={PreviewArticle}/>
+                <Route path="/components/academicUser/search" component={SearchResult}/>
+                {/*<Route path="/components/academicUser/userProfile" component={}/>*/}
+                <Route path="/components/academicUser/viewArticle" component={ViewArticle}/>
+                <Route path="/components/academicUser/viewUniversityProfile" component={UniversityProfile}/>
+
+
+                <Route path="/components/academicUser/Notifications/AllNotifications" component={AllNotifications}/>
+                <Route path="/components/academicUser/ProfileUpdate/UpdateProfile" component={UpdateProfile}/>
+                <Route path="/components/academicUser/ProfileInfo" component={ProfileInfo}/>
+                <Route path="/components/academicUser/AuthorProfile" component={AuthorProfile}/>
+
+            </Switch>
+        </Router>
+    )
+}
---
+++ src/components/academicUser/ProfileInfo.js
@@ -0,0 +1,197 @@
+import React, {useEffect, useState} from 'react';
+import {Avatar, Button, Card, CardContent, Divider, Grid, makeStyles, Typography} from '@material-ui/core';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import Post from './Post'
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        marginTop: 100,
+        width: '60%',
+        marginLeft: 275,
+        borderRadius: 30,
+    },
+    avatar: {
+        backgroundColor: '#935FF9',
+        marginBottom: '20px',
+        marginLeft: '20px',
+        width: 80,
+        height: 80,
+    },
+    buttonStyle: {
+        backgroundColor: '#935FF9',
+        color: '#FFFFFF',
+        marginTop: 30,
+        '&:hover': {
+            backgroundColor: '#4411A8',
+        },
+        marginBottom: '20px'
+    },
+    linkStyles: {
+        color: '#FFFFFF',
+        textDecoration: 'none',
+        '&:hover': {
+            color: '#FFFFFF',
+            textDecoration: 'none',
+        }
+    },
+    typographyStyle: {
+        textAlign: 'center',
+        fontSize: '16px'
+    },
+    title: {
+        textAlign: 'center',
+        // fontFamily: 'Courgette',
+        color: '#4411A8',
+    },
+    secondGrid: {
+        marginTop: 10,
+        marginLeft: 15,
+    }
+});
+
+function ProfileInfo() {
+
+    const [profileData, setProfileData] = useState([])
+    const logggedInUserId = '60ecfe51395a1704a42d8cae';
+    const userData = {"_id": logggedInUserId}
+    const url_loogedInUser = "http://localhost:9000/loggedIn_User";
+    useEffect(() => {
+        axios.post(url_loogedInUser, userData).then(function (response) {
+            setProfileData(response.data);
+        }).catch(function () {
+            console.error("Profile loading failed");
+        })
+    }, []);
+
+    const [postList, setpostList] = useState([])
+    const url_getUserPosts = "http://localhost:9000/loggedIn_User/get_all_publication";
+    useEffect(() => {
+        axios.post(url_getUserPosts, {user_id: logggedInUserId}).then(function (response) {
+            if (response.data)
+                setpostList(response.data);
+        }).catch(function () {
+            console.error("Posts loading failed");
+        })
+    }, []);
+    let postCount = 0;
+    postList.map(post => postCount = postCount + 1);
+
+    const displayPosts = postList.map(post => {
+        return (
+            <Post
+                author={profileData.name}
+                profilePic={profileData.profilePicture}
+                title={post.article.current.title}
+                coverImg={post.article.current.coverImage}
+                readTime={post.article.current.readTime}
+            />
+        )
+    })
+
+    const [tagstList, settagsList] = useState([])
+    const url_getUserTags = "http://localhost:9000/loggedIn_User/get_all_tags";
+    useEffect(() => {
+        axios.post(url_getUserTags, {user_id: logggedInUserId}).then(function (response) {
+            if (response.data)
+                settagsList(response.data);
+        }).catch(function () {
+            console.error("Tags loading failed");
+        })
+    }, []);
+    let tagsCount = 0;
+    tagstList.map(tag => tagsCount = tagsCount + 1);
+
+    const [followingUsers, setfollowingUsers] = useState([])
+    const url_getFollowingUsers = "http://localhost:9000/loggedIn_User/get_followingUsers";
+    useEffect(() => {
+        axios.post(url_getFollowingUsers, {user_id: logggedInUserId}).then(function (response) {
+            if (response.data)
+                setfollowingUsers(response.data);
+        }).catch(function () {
+            console.error("Following Users loading failed");
+        })
+    }, []);
+    let followingUserCount = 0;
+    followingUsers.map(followingUser => followingUserCount = followingUserCount + 1);
+
+    const [followedBy, setfollowedBy] = useState([])
+    const url_getFollowedBy = "http://localhost:9000/loggedIn_User/get_followedBy";
+    useEffect(() => {
+        axios.post(url_getFollowedBy, {user_id: logggedInUserId}).then(function (response) {
+            if (response.data)
+                setfollowedBy(response.data);
+        }).catch(function () {
+            console.error("Followed By loading failed");
+        })
+    }, []);
+    let followedByCount = 0;
+    followedBy.map(follower => followedByCount = followedByCount + 1);
+
+    return (
+        <div>
+            <div>
+                <Card className={useStyles().root}>
+
+                    <CardContent>
+                        <Grid container spacing={3} justifyContent="center">
+                            <Grid item>
+                                <Avatar alt="Profile image" className={useStyles().avatar}
+                                        src={profileData.profilePicture}/>
+                            </Grid>
+
+                            <Grid item>
+                                <Button aria-label="recipe" className={useStyles().buttonStyle}>
+                                    <Link className={useStyles().linkStyles}
+                                          to="/components/academicUser/UpdateProfile">
+                                        Edit Profile
+                                    </Link>
+                                </Button>
+                            </Grid>
+                        </Grid>
+
+                        <Typography gutterBottom variant="h5" component="h2" className={useStyles().title}>
+                            {profileData.name}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p"
+                                    className={useStyles().typographyStyle}>
+                            <p>{profileData.bio}</p>
+                            {/* <p>{university}</p> */}
+                            <p>{profileData.faculty}</p>
+                        </Typography>
+
+                    </CardContent>
+                </Card>
+            </div>
+
+            <div>
+                <Grid className={useStyles().secondGrid} container spacing={3} justifyContent="center">
+                    <Grid item>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="body2" color="textSecondary" component="p"
+                                            className={useStyles().typographyStyle}>
+                                    <p> Posts created: {postCount} </p>
+                                    <Divider/>
+                                    <p> Tags following: {tagsCount} </p>
+                                    <Divider/>
+                                    <p> Following authors: {followingUserCount} </p>
+                                    <Divider/>
+                                    <p> Followed by: {followedByCount} </p>
+                                    <Divider/>
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid item>
+                        {displayPosts}
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo
