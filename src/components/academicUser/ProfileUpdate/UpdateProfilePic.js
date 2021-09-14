4313274578ad83c45717103df048066a0cd28136
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-09-14 06:30:47 +0530

    Update UpdateProfilePic.js

--- src/components/academicUser/ProfileUpdate/UpdateProfilePic.js
+++ src/components/academicUser/ProfileUpdate/UpdateProfilePic.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {Button, Grid, makeStyles,} from '@material-ui/core';
 import axios from 'axios';
 import {DropzoneArea} from 'material-ui-dropzone'
@@ -26,22 +26,97 @@ const useStyles = makeStyles((theme) => ({
             backgroundColor: '#4411A8',
         },
         marginBottom: '20px',
-        width: '180px'
+        width: '180px',
+        marginLeft: '130px'
     },
     buttonStyleSub: {
-        backgroundColor: '#d81b60',
+        backgroundColor: '#9e9e9e',
         color: '#FFFFFF',
         '&:hover': {
-            backgroundColor: '#880E4F',
+            backgroundColor: '#d81b60',
         },
         marginBottom: '20px',
         width: '180px'
     },
 }));
 
-function UpdateProfilePic({userID, userProfilePic}) {
+function UpdateProfilePic({ userID }) {
     const classes = useStyles();
-    
+
+    // const [files, setfiles] = useState(null)
+    // const handlefileChange = ([file]) => {
+    //     file && setfiles(file)
+    //     console.log(files)
+    // }
+
+    // const urlUploadProfPic = 'http://localhost:9000/update_profilePic/uploadProfPic';
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+
+    //     const formData = new FormData();
+    //     formData.append("_id", userID);
+    //     formData.append("media", files);
+    //     console.log('photo upload try');
+
+    //     axios({
+    //         method: "put",
+    //         url: urlUploadProfPic,
+    //         data: formData,
+    //         headers: {"Content-Type": "multipart/form-data"},
+    //     })
+    //         .then(function (response) {
+    //             Swal.fire({
+    //                 icon: 'success',
+    //                 title: 'Profile picture is uploaded successfully',
+    //                 timer: 1500
+    //             })
+    //             console.log("Profile Pic updated successfully", "", "success");
+    //         })
+    //         .catch(function (err) {
+    //             Swal.fire({
+    //                 icon: 'error',
+    //                 title: 'Sorry!',
+    //                 text: 'Something went wrong. Try again later.'
+    //             })
+    //             console.log(err);
+    //         });
+    // }
+
+    // const urlRemoveProfPic = "http://localhost:9000/update_profilePic/removeProfPic";
+
+    // const removePhoto = () => {
+    //     Swal.fire({
+    //         title: 'Are you sure?',
+    //         text: "You won't be able to revert!",
+    //         icon: 'warning',
+    //         showCancelButton: true,
+    //         confirmButtonColor: '#3085d6',
+    //         cancelButtonColor: '#d33',
+    //         confirmButtonText: 'Yes, remove it!'
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             axios.delete(urlRemoveProfPic, userID).then(function (response) {
+    //                 console.log('Profile Pic is removed successfully');
+    //             })
+    //                 .then(function (response) {
+    //                     Swal.fire(
+    //                         'Deleted!',
+    //                         'Your image has been deleted.',
+    //                         'success'
+    //                     )
+    //                 })
+    //                 .catch(function (err) {
+    //                     Swal.fire({
+    //                         icon: 'error',
+    //                         title: 'Sorry!',
+    //                         text: 'Something went wrong. Try again later.'
+    //                     })
+    //                     console.log(err);
+    //                 });
+    //         }
+    //     })
+    // }
+
     return (
         <div>
             <form className={classes.root} noValidate autoComplete="off">
