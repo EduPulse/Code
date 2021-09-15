a70cc6994f7c44f053b7bca076c0d8b732768b4b
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-09-15 15:34:39 +0530

    Updated Home.js

--- src/components/academicUser/Home.js
+++ src/components/academicUser/Home.js
@@ -1,28 +1,25 @@
 import React from 'react'
-import {Grid, makeStyles} from '@material-ui/core';
-import Posts from '../posts';
+import { Grid, Paper, makeStyles } from '@material-ui/core';
 import './../../index.css';
 
 const useStyles = makeStyles((theme) => ({
-    maingrid: {
-        marginTop: '80px',
-        width: '80%',
-        marginLeft: 400,
-    }
+  maingrid:{
+    marginTop:'80px',
+    width:'80%',
+    marginLeft: 200,
+  }
 }));
 
 function Home() {
     const classes = useStyles();
     return (
-        <div>
-            <div align="center">
-                <Grid container spacing={3} className={classes.maingrid}>
-                    <Posts/>
-                    <Posts/>
-                    <Posts/>
-                </Grid>
-            </div>
+      <div>
+        <div align="center">
+            <Grid container spacing={3} className={classes.maingrid}>
+              <h4>WELCOME TO ACADEMIC USER HOME PAGE</h4>
+            </Grid>
         </div>
+      </div>
     )
 }
 
