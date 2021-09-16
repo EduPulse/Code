200b556effdfdf07dd5ca333b0c6fd5ba5173bfb
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-09-16 20:23:55 +0530

    Updated AllNotifications.js

--- src/components/academicUser/Notifications/AllNotifications.js
+++ src/components/academicUser/Notifications/AllNotifications.js
@@ -1,82 +1,17 @@
 import React, {useState} from 'react'
-
-import {Button, Grid, makeStyles} from '@material-ui/core';
-import 'bootstrap/dist/css/bootstrap.css';
+import {Button, Grid, makeStyles, Card} from '@material-ui/core';
 
 import CommentNotifications from './CommentNotifications'
 import NotificationsAllInOne from './NotificationsAllInOne'
 import PostNotifications from './PostNotifications'
 import ReactionNotifications from './ReactionNotifications'
+import ViewedNotifications from './ViewedNotifications';
+import NewNotifications from './NewNotifications';
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
         width: '80%',
-        marginTop: 100,
-    },
-    pubPostInfo: {
-        width: '80%',
-    },
-    postsInfo: {
-        width: '100%',
-    },
-    avatar: {
-        backgroundColor: '#935FF9',
-    },
-    headerInfo: {
-        marginTop: '90px',
-        marginBottom: '10px',
-        marginLeft: '110px',
-        width: '100%'
-    },
-    controlStyle: {
-        backgroundColor: '#C5B6E3',
-    },
-    buttonStyleCancel: {
-        backgroundColor: '#FA2C2C',
-        color: '#FFFFFF',
-        marginLeft: '20px',
-        width: '150px',
-        '&:hover': {
-            backgroundColor: '#A50000',
-        },
-        marginBottom: '20px'
-    },
-    buttonStyleMain: {
-        backgroundColor: '#4411A8',
-        color: '#FFFFFF',
-        '&:hover': {
-            backgroundColor: '#935FF9',
-        },
-        marginBottom: '20px'
-    },
-    buttonStyleSub: {
-        backgroundColor: '#b3b3cc',
-        color: '#FFFFFF',
-        '&:hover': {
-            backgroundColor: '#935FF9',
-        },
-        marginBottom: '20px'
-    },
-    buttonStyleSubmit: {
-        backgroundColor: '#4411A8',
-        color: '#FFFFFF',
-        paddingLeft: '20px',
-        width: '150px',
-        '&:hover': {
-            backgroundColor: '#935FF9',
-        },
-        marginBottom: '20px'
-    },
-    buttonStyleCancel: {
-        backgroundColor: '#FA2C2C',
-        color: '#FFFFFF',
-        marginLeft: '20px',
-        width: '150px',
-        '&:hover': {
-            backgroundColor: '#A50000',
-        },
-        marginBottom: '20px'
     },
     buttonStyle: {
         backgroundColor: '#935FF9',
@@ -85,91 +20,144 @@ const useStyles = makeStyles((theme) => ({
         '&:hover': {
             backgroundColor: '#4411A8',
         },
-        marginBottom: '10px'
+        '&:focus': {
+            backgroundColor: '#4411A8',
+        },
+        marginBottom: '10px',
+        marginLeft: '30px'
     },
-    linkStyles: {
-        color: '#FFFFFF',
-        textDecoration: 'none',
-        '&:hover': {
-            color: '#FFFFFF',
-            textDecoration: 'none',
-        }
-    }
+    cardStyles: {
+        backgroundColor: '#FFFFFF',
+        width: '96%',
+        marginLeft: '150px',
+        marginTop: '100px',
+        borderRadius: '5px',
+        marginBottom: '20px',
+        padding: 10,
+        paddingTop: 40
+    },
+    gridTwoStyle: {
+        marginTop: 10,
+        width: '100%',
+        marginBottom: '15px'
+    },
+    gridTwoItemOneStyle: {
+        marginLeft: '20px'
+    },
+    gridTwoItemTwoStyle: {
+        marginLeft: '20px'
+    },
 }));
 
-function TryFunct() {
+function AllNotifications() {
     const [all, setAll] = useState('block');
     const [post, setPost] = useState('none');
     const [comment, setComment] = useState('none');
     const [reaction, setReaction] = useState('none');
+    const [newNotifications, setnewNotifications] = useState('none')
+    const [viewedNotifications, setviewedNotifications] = useState('none');
 
     return (
-        <div>
-            <div align='center'>
-                <div className={useStyles().root}>
-                    <Grid container spacing={2} className={useStyles().pubPostInfo}>
-                        <Grid item xs className={useStyles().postsInfo}>
-                            <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                                setAll("block");
-                                setComment("none");
-                                setPost("none");
-                                setReaction("none");
-                            }}>
-                                All Notifications
-                            </Button>
+        <div className={useStyles().root}>
+            <Card className={useStyles().cardStyles} >
+                <Grid container spacing={2}  className={useStyles().gridTwoStyle} >
+                    <Grid item xs={3} className={useStyles().gridTwoItemOneStyle} >
+                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
+                            setAll("block");
+                            setComment("none");
+                            setPost("none");
+                            setReaction("none");
+                            setnewNotifications("none");
+                            setviewedNotifications("none");
+                        }}>
+                            All Notifications
+                        </Button>
 
-                            <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                                setAll("none");
-                                setComment("none");
-                                setPost("block");
-                                setReaction("none");
-                            }}>
-                                Publications
-                            </Button>
+                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
+                            setAll("none");
+                            setComment("none");
+                            setPost("block");
+                            setReaction("none");
+                            setnewNotifications("none");
+                            setviewedNotifications("none");
+                        }}>
+                            Publications
+                        </Button>
 
-                            <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                                setAll("none");
-                                setComment("block");
-                                setPost("none");
-                                setReaction("none");
-                            }}>
-                                Comments
-                            </Button>
+                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
+                            setAll("none");
+                            setComment("block");
+                            setPost("none");
+                            setReaction("none");
+                            setnewNotifications("none");
+                            setviewedNotifications("none");
+                        }}>
+                            Comments
+                        </Button>
 
-                            <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                                setAll("none");
-                                setComment("none");
-                                setPost("none");
-                                setReaction("block");
-                            }}>
-                                Reactions
-                            </Button>
-                        </Grid>
+                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
+                            setAll("none");
+                            setComment("none");
+                            setPost("none");
+                            setReaction("block");
+                            setnewNotifications("none");
+                            setviewedNotifications("none");
+                        }}>
+                            Reactions
+                        </Button>
 
-                        <Grid item xs={8} className={useStyles().postsInfo}>
-                            <Grid style={{display: all}}>
-                                <NotificationsAllInOne/>
-                            </Grid>
+                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
+                            setAll("none");
+                            setComment("none");
+                            setPost("none");
+                            setReaction("none");
+                            setnewNotifications("block");
+                            setviewedNotifications("none");
+                        }}>
+                            New Notifications
+                        </Button>
 
-                            <Grid style={{display: post}}>
-                                <PostNotifications/>
-                            </Grid>
-
-                            <Grid style={{display: comment}}>
-                                <CommentNotifications/>
-                            </Grid>
-
-                            <Grid style={{display: reaction}}>
-                                <ReactionNotifications/>
-                            </Grid>
-                        </Grid>
+                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
+                            setAll("none");
+                            setComment("none");
+                            setPost("none");
+                            setReaction("none");
+                            setnewNotifications("none");
+                            setviewedNotifications("block");
+                        }}>
+                            Viewed Notifications
+                        </Button>
                     </Grid>
 
-                </div>
-            </div>
+                    <Grid item xs={8} className={useStyles().gridTwoItemTwoStyle}>
+                        <Grid style={{display: all}}>
+                            <NotificationsAllInOne/>
+                        </Grid>
+
+                        <Grid style={{display: post}}>
+                            <PostNotifications/>
+                        </Grid>
+
+                        <Grid style={{display: comment}}>
+                            <CommentNotifications/>
+                        </Grid>
+
+                        <Grid style={{display: reaction}}>
+                            <ReactionNotifications/>
+                        </Grid>
+
+                        <Grid style={{display: newNotifications}}>
+                            <NewNotifications />
+                        </Grid>
+
+                        <Grid style={{display: viewedNotifications}}>
+                            <ViewedNotifications />
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Card>
         </div>
     );
 }
 
-export default TryFunct
-
+export default AllNotifications
