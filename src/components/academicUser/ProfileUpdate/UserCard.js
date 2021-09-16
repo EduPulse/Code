94f877b02004095aaea93f25e70ce599e9472f85
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-09-16 20:29:21 +0530

    Updated UserCard.js

--- src/components/academicUser/ProfileUpdate/UserCard.js
+++ src/components/academicUser/ProfileUpdate/UserCard.js
@@ -1,7 +1,6 @@
 import React, {useState, useEffect } from 'react'
 import axios from 'axios'
 import { Grid, makeStyles, Avatar, Card, Link } from '@material-ui/core';
-import AuthorProfile from '../AuthorProfile';
 
 const useStyles = makeStyles({
     root: {
