ba9fb5761d3ad657b8f87c6055cf1f67b9721160
Chathu Rashmini <64759099+chathuRashmini@users.noreply.github.com>
2021-09-17 20:55:49 +0530

    Updated AllNotifications.js

--- src/components/academicUser/Notifications/AllNotifications.js
+++ src/components/academicUser/Notifications/AllNotifications.js
@@ -1,12 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect } from 'react'
 import {Button, Grid, makeStyles, Card} from '@material-ui/core';
+import axios from 'axios';
 
 import CommentNotifications from './CommentNotifications'
-import NotificationsAllInOne from './NotificationsAllInOne'
 import PostNotifications from './PostNotifications'
 import ReactionNotifications from './ReactionNotifications'
-// import ViewedNotifications from './ViewedNotifications';
-// import NewNotifications from './NewNotifications';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -50,12 +48,43 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 function AllNotifications() {
-    const [all, setAll] = useState('block');
-    const [post, setPost] = useState('none');
+    
+    const [post, setPost] = useState('block');
     const [comment, setComment] = useState('none');
     const [reaction, setReaction] = useState('none');
-    const [newNotifications, setnewNotifications] = useState('none')
-    const [viewedNotifications, setviewedNotifications] = useState('none');
+
+    const [notications, setnotications] = useState([])
+    const userID = '60ecfe51395a1704a42d8cae';
+    const userData = {"_id": userID}
+    const url_notifications = "http://localhost:9000/loggedIn_User/get_notifications";
+    useEffect(() => {
+        axios.post(url_notifications, userData).then(function (response) {
+            setnotications(response.data);
+        }).catch(function () {
+        console.error("Notifications loading failed");
+        })
+    }, []);
+
+    const postNotifications = [];
+    const reactNotifications = [];
+    const commentNotifications = [];
+    const otherNotifications = [];
+
+    notications.map(notification => {
+        // console.log(notification.title);
+        if (notification.title === "publication") {
+            postNotifications.push(notification)
+        } 
+        else if (notification.title === "reaction") {
+            reactNotifications.push(notification)
+        }
+        else if (notification.title === "comment") {
+            commentNotifications.push(notification)
+        }
+        else {
+            otherNotifications.push(notification)
+        }
+    })
 
     return (
         <div className={useStyles().root}>
@@ -63,96 +92,48 @@ function AllNotifications() {
                 <Grid container spacing={2}  className={useStyles().gridTwoStyle} >
                     <Grid item xs={3} className={useStyles().gridTwoItemOneStyle} >
                         <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                            setAll("block");
-                            setComment("none");
-                            setPost("none");
-                            setReaction("none");
-                            setnewNotifications("none");
-                            setviewedNotifications("none");
-                        }}>
-                            All Notifications
-                        </Button>
-
-                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                            setAll("none");
                             setComment("none");
                             setPost("block");
                             setReaction("none");
-                            setnewNotifications("none");
-                            setviewedNotifications("none");
                         }}>
                             Publications
                         </Button>
 
                         <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                            setAll("none");
                             setComment("block");
                             setPost("none");
                             setReaction("none");
-                            setnewNotifications("none");
-                            setviewedNotifications("none");
                         }}>
                             Comments
                         </Button>
 
                         <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                            setAll("none");
                             setComment("none");
                             setPost("none");
                             setReaction("block");
-                            setnewNotifications("none");
-                            setviewedNotifications("none");
                         }}>
                             Reactions
-                        </Button>
-
-                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                            setAll("none");
-                            setComment("none");
-                            setPost("none");
-                            setReaction("none");
-                            setnewNotifications("block");
-                            setviewedNotifications("none");
-                        }}>
-                            New Notifications
-                        </Button>
-
-                        <Button aria-label="recipe" className={useStyles().buttonStyle} onClick={() => {
-                            setAll("none");
-                            setComment("none");
-                            setPost("none");
-                            setReaction("none");
-                            setnewNotifications("none");
-                            setviewedNotifications("block");
-                        }}>
-                            Viewed Notifications
                         </Button>
                     </Grid>
 
                     <Grid item xs={8} className={useStyles().gridTwoItemTwoStyle}>
-                        <Grid style={{display: all}}>
-                            <NotificationsAllInOne/>
-                        </Grid>
-
                         <Grid style={{display: post}}>
-                            <PostNotifications/>
+                            <PostNotifications
+                                postArray = {postNotifications}
+                            />
                         </Grid>
 
                         <Grid style={{display: comment}}>
-                            <CommentNotifications/>
+                            <CommentNotifications
+                                commentArray = {commentNotifications}
+                            />
                         </Grid>
 
                         <Grid style={{display: reaction}}>
-                            <ReactionNotifications/>
+                            <ReactionNotifications
+                                reactionArray = {reactNotifications}
+                            />
                         </Grid>
-
-                        // <Grid style={{display: newNotifications}}>
-                        //     <NewNotifications />
-                        // </Grid>
-
-                        // <Grid style={{display: viewedNotifications}}>
-                        //     <ViewedNotifications />
-                        // </Grid>
                     </Grid>
                 </Grid>
             </Card>
